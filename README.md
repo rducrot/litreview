d156d5fadce6255ef051f0073c208990faef3674
rducrot <ducrot.romain@gmail.com>
2022-11-21 15:48:32 +0100

    Implémentation login/logout simple

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LITReview
+[![Python](https://badgen.net/badge/Python/3.10/blue)](https://www.python.org/)
--- litreview/urls.py
+++ litreview/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
+import review.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(template_name='authentication/login.html',
+                               redirect_authenticated_user=True,), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('home/', review.views.home, name='home'),
 ]
