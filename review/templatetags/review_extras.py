8bead3c9cbdce622762212513a2ded45aa63146c
rducrot <ducrot.romain@gmail.com>
2022-12-02 10:41:14 +0100

    Filtres divers

--- review/templatetags/review_extras.py
+++ review/templatetags/review_extras.py
@@ -9,6 +9,11 @@ def model_type(value):
 
 
 @register.filter
+def format_date(time_created):
+    return f'{time_created.strftime("%H:%m, %d %B %Y")}'
+
+
+@register.filter
 def show_rating(rating):
     stars = ''
     for i in range(rating):
@@ -16,3 +21,24 @@ def show_rating(rating):
     for i in range(5 - rating):
         stars += '☆'
     return stars
+
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if user == context['user']:
+        return 'Vous'
+    return user.username
+
+
+@register.simple_tag(takes_context=True)
+def get_ticket_poster_display(context, user):
+    if user == context['user']:
+        return 'Vous avez demandé une critique.'
+    return f'{user.username} a demandé une critique.'
+
+
+@register.simple_tag(takes_context=True)
+def get_review_poster_display(context, user):
+    if user == context['user']:
+        return 'Vous avez publié une critique.'
+    return f'{user.username} a publié une critique.'
