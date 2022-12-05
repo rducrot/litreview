75a0c48c74a1764318bca79eb2acc390c9519fd6
rducrot <ducrot.romain@gmail.com>
2022-12-05 11:29:43 +0100

    TextInput pour suivre les utilisateurs avec suggestions

--- review/forms.py
+++ review/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
 
 from authentication.models import User
 from . import models
@@ -24,13 +25,22 @@ class ReviewForm(forms.ModelForm):
 
 
 class UserFollowsForm(forms.ModelForm):
-    def __init__(self, user, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, user, data=None, *args, **kwargs):
+        if data is not None:
+            # Data must be mutable
+            data = data.copy()
+            user_to_follow = get_object_or_404(User, username=data['followed_user'])
+            if data['followed_user']:
+                data['followed_user'] = user_to_follow.id
+
+        super().__init__(data=data, *args, **kwargs)
         # ensures we can only select other users we don't already follow
-        self.fields['followed_user'].queryset = User.objects.all().exclude(
+        self.choices = User.objects.all().exclude(
             Q(followed_by__in=user.following.all()) |
             Q(id=user.id)
-        )
+        ).values('username')
+        self.fields['followed_user'].widget = forms.TextInput(attrs={'list': 'followed_users'})
+
 
     class Meta:
         model = models.UserFollows
