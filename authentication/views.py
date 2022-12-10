1ccd739b7ea84f1493f5b4f369eca71a4cb0604c
rducrot <ducrot.romain@gmail.com>
2022-12-10 09:40:00 +0100

    Correction login

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import forms
 
@@ -8,5 +8,11 @@ def signup(request):
     Signup page.
     """
     form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
     return render(request, 'authentication/signup.html',
                   context={'form': form})
